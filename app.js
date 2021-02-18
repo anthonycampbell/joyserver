3bd6ebadf99a11997057a5c89ca6bf8b65d0460f
anthonycampbell <anthony-campbell@hotmail.com>
2021-02-18 00:48:46 -0800

    cors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ require('./config/passport')(passport);
 var corsOpts = {
   origin: 'https://calm-tundra-99790.herokuapp.com',
   credentials: true,
-  methods: 'GET,PUT,POST,DELETE,OPTIONS,HEAD,authorization',
+  methods: 'GET,PUT,POST,DELETE,OPTIONS,HEAD,authorization, Access-Control-Allow-Headers',
   allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Access-Control-Allow-Headers,Access-Control-Request-Method,Access-Control-Request-Headers'
 }
 app.use(cors(corsOpts));
